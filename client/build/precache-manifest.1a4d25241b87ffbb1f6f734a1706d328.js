bd111a6e64795c941f53718a104fda727e38909c
hozerpffp <hozerpffp2009@gmail.com>
2020-09-14 21:26:27 -0500

    added npm cors to server folder

---
+++ client/build/precache-manifest.1a4d25241b87ffbb1f6f734a1706d328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0914eb3abe5c31906c7c",
+    "url": "/static/css/main.c890b039.chunk.css"
+  },
+  {
+    "revision": "0914eb3abe5c31906c7c",
+    "url": "/static/js/main.28014630.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "752083936c9510c13a40",
+    "url": "/static/js/2.12c04363.chunk.js"
+  },
+  {
+    "revision": "86d12a3caa03448aacd43c14eac41040",
+    "url": "/index.html"
+  }
+];
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,34 +1,35 @@
 import React from "react";
 import API from "../utils/API";
-import Results from "../components/Results"
+import Results from "../components/Results";
 
 class Saved extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            savedBooks: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      savedBooks: [],
+    };
+  }
 
-    componentWillMount() {
-        API.getBooks().then(
-            (response) => {
-                this.setState({savedBooks: response.data});
-            }
-        ) .catch(
-            (err) => {
-                console.log("Saved Books error", err);
-            }
-        );
-    }
+  componentWillMount() {
+    API.getBooks()
+      .then((response) => {
+        this.setState({savedBooks: response.data});
+      })
+      .catch((err) => {
+        console.log("Saved Books error", err);
+      });
+  }
 
-    render() {
-        return(
-            <main>
-                <Results saveBooks={this.state.savedBooks} path={this.props.match.path} />
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main>
+        <Results
+          saveBooks={this.state.savedBooks}
+          path={this.props.match.path}
+        />
+      </main>
+    );
+  }
 }
 
 export default Saved;
