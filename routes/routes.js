1d7a54e5b2596e98b429d34cfb3263557a011f11
hozerpffp <hozerpffp2009@gmail.com>
2020-09-24 18:12:37 -0500

    added console.log

--- routes/routes.js
+++ routes/routes.js
@@ -3,60 +3,53 @@ const axios = require("axios");
 const db = require("../models");
 const path = require("path");
 
-module.exports = function(app) {
-    app.get("/api/book", (req, res) => {
-        db.Book.find().then(
-            (bookData) => {
-                res.json(bookData);
-            }
-        ) .catch(
-            (err) => {
-                res.json({error: err});
-            }
-        );
-    });
-    
-    app.post("/search", (req, res) => {
-        let bookTitle = req.body.title.replace(/\s/g, "+");
-        axios.get(
-            `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=${process.env.GBOOKS_KEY}`
-        ) .then(
-            (response) => {
-                res.json(response.data.items)
-            }
-        ) .catch(
-            (err) => {
-                res.json({error: err})
-            }
-        );
-    });
+module.exports = function (app) {
+  app.get("/api/book", (req, res) => {
+    db.Book.find()
+      .then((bookData) => {
+        res.json(bookData);
+      })
+      .catch((err) => {
+        res.json({error: err});
+      });
+  });
 
-    app.post("/api/book", (req, res) => {
-        //database books-models accessing- .create passing req.body
-        db.Book.create(req.body).then(
-            (response) => {
-                res.json({successful: response});
-            }
-        ).catch(
-            (err) => {
-                res.json({error: err});
-            }                                
-        );
-    });
+  app.post("/search", (req, res) => {
+    let bookTitle = req.body.title.replace(/\s/g, "+");
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&key=${process.env.GBOOKS_KEY}`
+      )
+      .then((response) => {
+        res.json(response.data.items);
+      })
+      .catch((err) => {
+        res.json({error: err});
+      });
+  });
 
-    app.delete("/api/book/:id", (req, res) => {
-        db.Book.findByIdAndDelete(req.params.id).then(
-            (response) => {
-                res.json({successful: response});
-            }
-        ).catch(
-            (err) => {
-                rres.json({error: err});
-            }
-        );
-    });
+  app.post("/api/book", (req, res) => {
+    //database books-models accessing- .create passing req.body
+    db.Book.create(req.body)
+      .then((response) => {
+        res.json({successful: response});
+      })
+      .catch((err) => {
+        res.json({error: err});
+      });
+  });
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/build/index.html"));
-        });
-}
+  app.delete("/api/book/:id", (req, res) => {
+    db.Book.findByIdAndDelete(req.params.id)
+      .then((response) => {
+        res.json({successful: response});
+      })
+      .catch((err) => {
+        rres.json({error: err});
+      });
+  });
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+};
